80517e3e958ace9e4ef300051155ba4a8033431d
Marcos Corrochano Arroyo <mcorrochanoa@outlook.com>
2025-11-11 00:09:47 +0100

    pre-commit hook

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,22 @@
 // @ts-check
-import { defineConfig } from 'astro/config'
+import { defineConfig, fontProviders } from 'astro/config'
 import sitemap from '@astrojs/sitemap'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://roikat.github.io',
-  integrations: [sitemap()]
+  integrations: [sitemap()],
+  experimental: {
+    fonts: [
+      {
+        cssVariable: '--font-lexend',
+        name: 'Lexend',
+        provider: fontProviders.google()
+      }
+    ]
+  },
+  vite: {
+    plugins: [tailwindcss()]
+  }
 })
